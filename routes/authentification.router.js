237085cc494e428c9dcc80caf4e14a7a8c5ae30a
yassinbnsalah <yacinbnsalh@gmail.com>
2024-11-02 22:28:22 +0100

    StableV

--- routes/authentification.router.js
+++ routes/authentification.router.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import User from '../models/user.js';
+import axios from "axios";
 
 
 const router = express.Router();
@@ -13,10 +14,11 @@ router.post("/login", async (req, res) => {
             let user = await User.findOne({ googleId });
 
             if (!user) {
-                user = new User({ googleId, imageUrl, email, name, givenName, familyName });
+                const role = "Client";
+                user = new User({ googleId, imageUrl, email, name, givenName, familyName , role });
                 await user.save();
             }
-
+            await axios.post(`http://localhost:3000/notification/newauth/${email}`);
             res.status(200).json(user);
         } catch (error) {
             res.status(500).json({ message: error.message });
@@ -26,6 +28,14 @@ router.post("/login", async (req, res) => {
     }
 });
 
+router.get("/all/users", async (req, res) => {
+    try {
+        let users = await User.find({ role: "Client" });
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 
 
