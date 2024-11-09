4e94f33edc8eebcc945a9560c89a1467ef31e120
yassinbnsalah <yacinbnsalh@gmail.com>
2024-11-09 14:31:54 +0100

    VF

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import authRouter from './routes/authentification.router.js';
-import bodyParser from 'body-parser';
 const app = express();
 import connectToMongoDB from './database/connection.js';
 import { swaggerUi, swaggerDocs } from './docsconfig/swaggerConfig.js';
@@ -10,19 +9,11 @@ connectToMongoDB();
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
-// Set default route for '/'
-app.get('/', (req, res) => {
-  res.send('Welcome to the MS Account URL!');
-});
-app.use("/", authRouter);
 
-app.get('/test', (req, res) => {
-  res.send('Welcome to the MS Account URL!');
-});
+app.use("/", authRouter);
 
 app.listen(3001, () => {
   console.log(`Server is running at http://localhost:3001`);
 });
 
-// Export the app instance
 export default app;
