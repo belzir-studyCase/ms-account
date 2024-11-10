67d8e92267759c5c4660334fab03fbff9cfb2389
yassinbnsalah <yacinbnsalh@gmail.com>
2024-11-10 20:02:57 +0100

    SecuredLinks

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ import authRouter from './routes/authentification.router.js';
 const app = express();
 import connectToMongoDB from './database/connection.js';
 import { swaggerUi, swaggerDocs } from './docsconfig/swaggerConfig.js';
-
+import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 connectToMongoDB();
-
-
+app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 
 app.use("/", authRouter);
 
-app.listen(3001, () => {
-  console.log(`Server is running at http://localhost:3001`);
+https.createServer(options, app).listen(3001, () => {
+  console.log('HTTPS server running on https://localhost:3001');
 });
 
-export default app;
+
